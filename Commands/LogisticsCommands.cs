1fb96bad07fd25443ee8c3a63ffead5944abd19a
toretgman <82327757+toretgman@users.noreply.github.com>
2024-05-27 19:03:59 +0800

    Update LogisticsCommands.cs

--- Commands/LogisticsCommands.cs
+++ Commands/LogisticsCommands.cs
@@ -8,49 +8,49 @@ namespace Logistics.Commands
     [CommandGroup(name: "logistics", "l")]
     public static class LogisticsCommands
     {
-        [Command(name: "sortstash", shortHand: "ss", usage: ".l ss", description: "Toggles autostashing on double clicking sort button for player.")]
+        [Command(name: "遠程存物", shortHand: "ss", usage: ".l ss", description: "雙擊排序按鈕存入倉庫")]
         public static void TogglePlayerAutoStash(ChatCommandContext ctx)
         {
             var SteamID = ctx.Event.User.PlatformId;
 
             var autoStash = Core.PlayerSettings.ToggleSortStash(SteamID);
-            ctx.Reply($"SortStash is {(autoStash ? "<color=green>enabled</color>" : "<color=red>disabled</color>")}.");
+            ctx.Reply($"SortStash is {(autoStash ? "<color=green>已啟動</color>" : "<color=red>已關閉</color>")}.");
         }
        
-        [Command(name: "craftpull", shortHand: "cr", usage: ".l cr", description: "Toggles right-clicking on recipes for missing ingredients.")]
+        [Command(name: "遠程取物", shortHand: "cr", usage: ".l cr", description: "右鍵點擊製作道具以查找傳送缺少的材料")]
         public static void TogglePlayerAutoPull(ChatCommandContext ctx)
         {
             var SteamID = ctx.Event.User.PlatformId;
 
             var autoPull = Core.PlayerSettings.ToggleCraftPull(SteamID);
-            ctx.Reply($"CraftPull is {(autoPull ? "<color=green>enabled</color>" : "<color=red>disabled</color>")}.");
+            ctx.Reply($"CraftPull is {(autoPull ? "<color=green>已啟動</color>" : "<color=red>已關閉</color>")}.");
         }
 
-        [Command(name: "dontpulllast", shortHand: "dpl", usage: ".l dpl", description: "Toggles the ability to not pull the last item from a container for Logistics commands.")]
+        [Command(name: "dontpulllast", shortHand: "dpl", usage: ".l dpl", description: "Toggles the ability to not pull the last item from a container for Logistics commands.", adminOnly: true)]
         public static void ToggleDontPullLast(ChatCommandContext ctx)
         {
             var SteamID = ctx.Event.User.PlatformId;
 
             var dontPullLast = Core.PlayerSettings.ToggleDontPullLast(SteamID);
-            ctx.Reply($"DontPullLast is {(dontPullLast ? "<color=green>enabled</color>" : "<color=red>disabled</color>")}.");
+            ctx.Reply($"DontPullLast is {(dontPullLast ? "<color=green>已啟動</color>" : "<color=red>已關閉</color>")}.");
         }
 
-        [Command(name: "autostashmissions", shortHand: "asm", usage: ".l asm", description: "Toggles autostashing for servant missions.")]
+        [Command(name: "autostashmissions", shortHand: "asm", usage: ".l asm", description: "Toggles autostashing for servant missions.", adminOnly: true)]
         public static void ToggleServantAutoStash(ChatCommandContext ctx)
         {
             var SteamID = ctx.Event.User.PlatformId;
 
             var autoStashMissions = Core.PlayerSettings.ToggleAutoStashMissions(SteamID);
-            ctx.Reply($"AutoStash for missions is {(autoStashMissions ? "<color=green>enabled</color>" : "<color=red>disabled</color>")}.");
+            ctx.Reply($"AutoStash for missions is {(autoStashMissions ? "<color=green>已啟動</color>" : "<color=red>已關閉</color>")}.");
         }
 
-        [Command(name: "conveyor", shortHand: "co", usage: ".l co", description: "Toggles the ability of sender/receiver's to move items around.")]
+        [Command(name: "conveyor", shortHand: "co", usage: ".l co", description: "Toggles the ability of sender/receiver's to move items around.", adminOnly: true)]
         public static void ToggleConveyor(ChatCommandContext ctx)
         {
             var SteamID = ctx.Event.User.PlatformId;
 
             var conveyor = Core.PlayerSettings.ToggleConveyor(SteamID);
-            ctx.Reply($"Conveyor is {(conveyor ? "<color=green>enabled</color>" : "<color=red>disabled</color>")}.");
+            ctx.Reply($"Conveyor is {(conveyor ? "<color=green>已啟動</color>" : "<color=red>已關閉</color>")}.");
         }
 
         [Command(name: "settings", shortHand: "s", usage: ".l s", description: "Displays current settings.")]
@@ -61,12 +61,12 @@ namespace Logistics.Commands
             var settings = Core.PlayerSettings.GetSettings(SteamID);
             var globalSettings = Core.PlayerSettings.GetGlobalSettings();
             ctx.Reply("KindredLogistics settings:\n" +
-                      $"SortStash{(globalSettings.SortStash ? "" : "(<color=red>Server Disabled</color>)")}: {(settings.SortStash ? "<color=green>enabled</color>" : "<color=red>disabled</color>")}\n" +
+                      $"SortStash{(globalSettings.SortStash ? "" : "(<color=red>Server Disabled</color>)")}: {(settings.SortStash ? "<color=green>已啟動</color>" : "<color=red>已關閉</color>")}\n" +
                       $"Pull (Global) : {(globalSettings.Pull ? "<color=green>enabled</color>" : "<color=red>disabled</color>")}\n" +
-                      $"CraftPull{(globalSettings.CraftPull ? "" : "(<color=red>Server Disabled</color>)")}: {(settings.CraftPull ? "<color=green>enabled</color>" : "<color=red>disabled</color>")}\n" +
+                      $"CraftPull{(globalSettings.CraftPull ? "" : "(<color=red>Server Disabled</color>)")}: {(settings.CraftPull ? "<color=green>已啟動</color>" : "<color=red>已關閉</color>")}\n" +
                       $"DontPullLast: {(settings.DontPullLast ? "<color=green>enabled</color>" : "<color=red>disabled</color>")}\n" +
-                      $"AutoStashMissions{(globalSettings.AutoStashMissions ? "" : "(<color=red>Server Disabled</color>)")}: {(settings.AutoStashMissions ? "<color=green>enabled</color>" : "<color=red>disabled</color>")}\n" +
-                      $"Conveyor{(globalSettings.Conveyor ? "" : "(<color=red>Server Disabled</color>)")}: {(settings.Conveyor ? "<color=green>enabled</color>" : "<color=red>disabled</color>")}"
+                      $"AutoStashMissions{(globalSettings.AutoStashMissions ? "" : "(<color=red>Server Disabled</color>)")}: {(settings.AutoStashMissions ? "<color=green>已啟動</color>" : "<color=red>已關閉</color>")}\n" +
+                      $"Conveyor{(globalSettings.Conveyor ? "" : "(<color=red>Server Disabled</color>)")}: {(settings.Conveyor ? "<color=green>已啟動</color>" : "<color=red>已關閉</color>")}"
                       );
         }  
     }
@@ -126,19 +126,19 @@ namespace Logistics.Commands
 
     public static class AdditionalCommands
     {
-        [Command(name: "存入倉庫", description: "你背包的道具以幫您傳送回該所屬的倉庫了.")]
+        [Command(name: "stash", shortHand: "存入倉庫", description: "你背包的道具以幫您傳送回該所屬的倉庫了.")]
         public static void StashInventory(ChatCommandContext ctx)
         {
             Core.Stash.StashCharacterInventory(ctx.Event.SenderCharacterEntity);
         }
 
-        [Command(name: "pull", description: "Pulls specified item from containers.")]
+        [Command(name: "pull", description: "從倉庫中取出指定的道具.")]
         public static void PullItem(ChatCommandContext ctx, FoundItem item, int quantity = 1)
         {
             PullService.PullItem(ctx.Event.SenderCharacterEntity, item.prefab, quantity);
         }
 
-        [Command(name: "finditem", shortHand: "fi", description: "Finds the specified item in containers")]
+        [Command(name: "finditem", shortHand: "fi", description: "尋找倉庫中指定的道具")]
         public static void FindItem(ChatCommandContext ctx, FoundItem item)
         {
             Core.Stash.ReportWhereItemIsLocated(ctx.Event.SenderCharacterEntity, item.prefab);
