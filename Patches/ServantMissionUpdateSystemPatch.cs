1afffbb31e46653b768d317b1859bcec980a559a
zfolmt <mitchell.foltz@gmail.com>
2024-06-04 02:53:05 -0500

    Update ServantMissionUpdateSystemPatch.cs
    small change to servant processing

--- Patches/ServantMissionUpdateSystemPatch.cs
+++ Patches/ServantMissionUpdateSystemPatch.cs
@@ -11,12 +11,10 @@ namespace KindredLogistics.Patches;
 [HarmonyPatch(typeof(ServantMissionUpdateSystem), nameof(ServantMissionUpdateSystem.OnUpdate))]
 public static class ServantMissionUpdateSystemPatch
 {
-    static readonly Dictionary<Entity, bool> wasDisabled = [];
     static void Prefix(ServantMissionUpdateSystem __instance)
     {
         var missions = __instance._TempFinishedMissions;
         var servants = __instance._TempServantList;
-        List<Entity> missionServants = [];
         try
         {
             foreach (var mission in missions)
@@ -24,32 +22,15 @@ public static class ServantMissionUpdateSystemPatch
                 var owner = mission.MissionOwner.Read<UserOwner>().Owner._Entity;
                 var steamId = owner.Read<User>().PlatformId;
                 if (!Core.PlayerSettings.IsAutoStashMissionsEnabled(steamId)) continue;
-                
-                foreach (var servant in missionServants)
+                foreach (var servant in servants)
                 {
                     Utilities.StashServantInventory(servant);
-                    RestoreDisabled(servant);
                 }
             }
         }
         catch (Exception e)
         {
             Core.Log.LogError($"Exited ServantMissionActionSystem hook early: {e}");
-        }
-    }
-    static void HandleDisabled(Entity entity, bool disabled)
-    {
-        entity.Remove<DisableWhenNoPlayersInRange>();
-        entity.Remove<DisabledDueToNoPlayersInRange>();
-        wasDisabled[entity] = disabled;
-    }
-    static void RestoreDisabled(Entity entity)
-    {
-        if (wasDisabled.TryGetValue(entity, out var disabled) && disabled)
-        {
-            entity.Add<DisableWhenNoPlayersInRange>();
-            entity.Add<DisabledDueToNoPlayersInRange>();
-            wasDisabled.Remove(entity);
         }
     }
 }
