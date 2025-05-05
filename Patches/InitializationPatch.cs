ca517ed59a923654493e192afda43090f8b0e62b
Amber <odjiti@gmail.com>
2025-05-04 22:51:31 -0400

    Versioning, init patch update

--- Patches/InitializationPatch.cs
+++ Patches/InitializationPatch.cs
@@ -3,13 +3,13 @@ using Unity.Scenes;
 
 namespace KindredLogistics.Patches;
 
-[HarmonyPatch(typeof(SceneSystem), nameof(SceneSystem.ShutdownStreamingSupport))]
+[HarmonyPatch(typeof(SceneSectionStreamingSystem), nameof(SceneSectionStreamingSystem.ShutdownAsynchrnonousStreamingSupport))]
 public static class InitializationPatch
 {
     [HarmonyPostfix]
     public static void OneShot_AfterLoad_InitializationPatch()
     {
         Core.Initialize();
-        Plugin.Harmony.Unpatch(typeof(SceneSystem).GetMethod("ShutdownStreamingSupport"), typeof(InitializationPatch).GetMethod("OneShot_AfterLoad_InitializationPatch"));
+        Plugin.Harmony.Unpatch(typeof(SceneSectionStreamingSystem).GetMethod("ShutdownAsynchrnonousStreamingSupport"), typeof(InitializationPatch).GetMethod("OneShot_AfterLoad_InitializationPatch"));
     }
 }
